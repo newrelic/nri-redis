2fc840eb1f7b0b1514441c356067bc46df439c3b
Joan Porta <jportasa@gmail.com>
2020-09-25 16:12:07 +0200

    fix: Add create and upload win zip

--- build/create_upload_win_zip.sh
+++ build/create_upload_win_zip.sh
@@ -10,7 +10,7 @@ ARCH=$2
 TAG=$3
 GITHUB_TOKEN=$4
 
-echo "===> Creating zip nri-${INTEGRATIONNAME}-${ARCH}.${TAG}.zip"
+echo "===> Creating zip nri-${INTEGRATIONNAME}-${ARCH}.${TAG:1}.zip"
 mkdir -p zip/'New Relic'/'newrelic-infra'/'newrelic-integrations'/bin/
 mkdir -p zip/'New Relic'/'newrelic-infra'/'integrations.d'/
 
@@ -19,8 +19,8 @@ cp ${INTEGRATIONNAME}-definition.yml zip/'New Relic'/'newrelic-infra'/'newrelic-
 cp ${INTEGRATIONNAME}-config.yml.sample zip/'New Relic'/'newrelic-infra'/'integrations.d'/
 
 cd zip
-7z a -r nri-${INTEGRATIONNAME}-${ARCH}.${TAG}.zip .
+7z a -r nri-${INTEGRATIONNAME}-${ARCH}.${TAG:1}.zip .
 
-echo "===> Pushing nri-${INTEGRATIONNAME}-${ARCH}.${TAG}.zip to GHA Release assets"
+echo "===> Pushing nri-${INTEGRATIONNAME}-${ARCH}.${TAG:1}.zip to GHA Release assets"
 export $GITHUB_TOKEN
 hub release edit -a nri-${INTEGRATIONNAME}-${ARCH}.${TAG}.zip -m "${TAG}" ${TAG}
