3a3a0d347a44ac6008420e376a7c120d6c770863
Joan Porta <jportasa@gmail.com>
2020-09-28 10:47:05 +0200

    Fix: exe metadata

--- build/set_exe_properties.sh
+++ build/set_exe_properties.sh
@@ -25,9 +25,6 @@ sed \
   -e "s/{PatchVersion}/$PatchVersion/g" \
   -e "s/{BuildVersion}/$BuildVersion/g" versioninfo.json.template > src/versioninfo.json
 
-echo "===> Checking versioninfo.json is in the path"
-cat src/versioninfo.json
-
 export PATH="$PATH:/go/bin"
 go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
 go generate github.com/newrelic/nri-redis/cmd/
