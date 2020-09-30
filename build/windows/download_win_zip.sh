6a155a49bc50c2dd437a98b194e59755b2c47268
Grigorii Merkushev <brushknight@gmail.com>
2020-09-30 12:14:06 +0200

    feat: add win zip unpack for msi

--- build/windows/download_win_zip.sh
+++ build/windows/download_win_zip.sh
@@ -12,4 +12,5 @@ INTEGRATION=$2
 zip_name="nri-${INTEGRATION}_windows_${TAG:1}_${ARCH}.zip"
 wget -O ./dist --quiet https://github.com/${REPO_FULL_NAME}/releases/download/${TAG}/${zip_name}
 
-7z e "dist/${zip_name}" -o "./dist/nri-${INTEGRATION}_windows_${TAG:1}_${ARCH}/"
+7z e "dist/${zip_name}" -o "dist/nri-${INTEGRATION}_windows_${TAG:1}_${ARCH}/"
+cp "dist/nri-${INTEGRATION}_windows_${TAG:1}_${ARCH}/New Relic/newrelic-infra/newrelic-integrations/bin/nri-${INTEGRATION}.exe" dist/
