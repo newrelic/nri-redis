36bb26bf583e35b9577f2942c2227909264f8cfc
Grigorii Merkushev <brushknight@gmail.com>
2020-09-30 17:19:14 +0200

    fix: fix win msi packaging step

--- build/windows/download_zip_extract_exe.ps1
+++ build/windows/download_zip_extract_exe.ps1
@@ -5,10 +5,14 @@ param (
     [string]$REPO_FULL_NAME="none"
 )
 $VERSION=${TAG}.substring(1)
+$exe_folder="nri-${INTEGRATION}_windows_${ARCH}"
 $zip_name="nri-${INTEGRATION}_windows_${VERSION}_${ARCH}.zip"
 
-write-host "===> Downloading & extracting .exe from https://github.com/${REPO_FULL_NAME}/releases/download/${TAG}/${zip_name}"
-
-Invoke-WebRequest "https://github.com/${REPO_FULL_NAME}/releases/download/${TAG}/${zip_name}" -OutFile ".\dist\${zip_name}"
-expand-archive -path '.\dist\${zip_name}' -destinationpath .\dist\
-Copy-Item -Path ".\dist\New Relic\newrelic-infra\newrelic-integrations\bin\nri-${INTEGRATION}.exe" -Destination ".\dist\" -Force
+$zip_url="https://github.com/${REPO_FULL_NAME}/releases/download/${TAG}/${zip_name}"
+write-host "===> Downloading & extracting .exe from ${zip_url}"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest "${zip_url}" -OutFile ".\dist\${zip_name}"
+write-host "===> Expanding"
+expand-archive -path "dist\${zip_name}" -destinationpath "dist\${exe_folder}\"
+write-host "===> copying"
+Copy-Item -Path ".\dist\${exe_folder}\New Relic\newrelic-infra\newrelic-integrations\bin\nri-${INTEGRATION}.exe" -Destination "dist\${exe_folder}\" -Force
