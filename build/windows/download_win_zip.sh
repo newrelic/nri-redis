21ea9e078ee7dd26886b7f8a34b60a3fdf7c31f2
Grigorii Merkushev <brushknight@gmail.com>
2020-09-30 10:10:34 +0200

    feat: working on building msi

--- build/windows/download_win_zip.sh
+++ build/windows/download_win_zip.sh
@@ -7,8 +7,9 @@ set -e
 #
 
 ARCH=$1
+INTEGRATION=$2
 
-zip_name="nri-redis_windows_${TAG:1}_${ARCH}.zip"
+zip_name="nri-${INTEGRATION}_windows_${TAG:1}_${ARCH}.zip"
 wget -O ./dist --quiet https://github.com/${REPO_FULL_NAME}/releases/download/${TAG}/${zip_name}
 
-7z e "dist/${zip_name}" -o ./dist
+7z e "dist/${zip_name}" -o "./dist/nri-${INTEGRATION}_windows_${TAG:1}_${ARCH}/"
