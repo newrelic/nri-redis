e2b82dc03b9445175af16010427c33998e5b0c1b
Joan Porta <jportasa@gmail.com>
2020-10-02 16:05:48 +0200

    Param not passed to the script

--- build/windows/upload_msi.sh
+++ build/windows/upload_msi.sh
@@ -5,7 +5,8 @@ set -e
 # Gets dist/zip_dirty created by Goreleaser and reorganize inside files
 #
 #
-ARCH=$1
-TAG=$2
+INTEGRATION=$1
+ARCH=$2
+TAG=$3
 
-hub release edit -a "build/package/windows/nri-${ARCH}-installer/bin/Release/nri-$(INTEGRATION)-${ARCH}.${TAG:1}.msi" -m ${TAG} ${TAG}
+hub release edit -a "build/package/windows/nri-${ARCH}-installer/bin/Release/nri-${INTEGRATION}-${ARCH}.${TAG:1}.msi" -m ${TAG} ${TAG}
