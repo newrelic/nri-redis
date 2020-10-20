1ad8cec9724c6b45febded427670d3d20c27c30a
Joan Porta <jportasa@gmail.com>
2020-10-20 11:19:32 +0200

    Ci/pipeline GitHub token issue (#90)
    * Fix Github token upload artifact to GH failure
    
    * Fix Github token upload artifact to GH failure
    
    * Fix Github token upload artifact to GH failure

--- build/upload_artifacts_gh.sh
+++ build/upload_artifacts_gh.sh
@@ -7,5 +7,9 @@
 cd dist
 for package in $(find  -regex ".*\.\(msi\|rpm\|deb\|zip\|tar.gz\)");do
   echo "===> Uploading to GH $TAG: ${package}"
+  echo "===> Debugging"
+  echo "GITHUB_TOKEN: $GITHUB_TOKEN"
+  echo "GITHUB_USER: $GITHUB_USER"
+  echo "===> END Debugging"
   hub release edit -a ${package} -m "${TAG}" ${TAG}
 done
