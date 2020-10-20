d3f33213c66206e443875c981a667b5cfc900a0d
Joan Porta <jportasa@gmail.com>
2020-10-20 12:20:34 +0200

    Replace curl to upload by gh CLI

--- build/upload_artifacts_gh.sh
+++ build/upload_artifacts_gh.sh
@@ -6,13 +6,9 @@ set -e
 #
 #
 cd dist
-release_id=$(curl --header "authorization: Bearer $GITHUB_TOKEN" --url https://api.github.com/repos/${REPO_FULL_NAME}/releases/tags/${TAG} | jq --raw-output '.id' )
-
 for filename in $(find  -regex ".*\.\(msi\|rpm\|deb\|zip\|tar.gz\)");do
   echo "===> Uploading to GH $TAG: ${filename}"
-  curl -s \
-       -H "Authorization: token $GITHUB_TOKEN" \
-       -H "Content-Type: application/octet-stream" \
-       --data-binary @$filename \
-       "https://uploads.github.com/repos/${REPO_FULL_NAME}/releases/${release_id}/assets?name=$(basename $filename)"
+      gh release upload $TAG $filename
 done
+
+
