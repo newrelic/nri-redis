c047e4a8689937bdf0d2e5f4d0437c8166d37b87
Joan Porta <jportasa@gmail.com>
2020-10-27 08:50:34 +0100

    fix: cicd, testing S3 upload to S3

--- build/docker/dockerfiles/yum-updater-s3fuse/entrypoint.sh
+++ build/docker/dockerfiles/yum-updater-s3fuse/entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 #
 #
-# Mount S3 with S3Fuse and update YUM/ZYPP repo
+# Mount S3 with S3Fuse and update YUM repo
 #
 #
 OS_VERSIONS=( 5 6 7 8 )
-ARCH=( x86_64 arm arm64)
+ARCH=( x86_64 arm arm64 )
+SUFIX='1'
 
 [ "${DEBUG:-false}" == 'true' ] && { set -x; S3FS_DEBUG='-d -d'; }
 
@@ -39,31 +40,42 @@ s3fs $S3FS_DEBUG $S3FS_ARGS -o passwd_file=${AWS_S3_AUTHFILE} -o url=${AWS_S3_UR
 echo "===> Importing GPG signature"
 printf %s ${GPG_PRIVATE_KEY_BASE64} | base64 --decode | gpg --batch --import -
 
-echo "===> Download packages from GH and uploading to S3"
-for os_version in "${OS_VERSIONS[@]}"; do
-  package_name="newrelic-infra-${TAG:1}.el${os_version}.${ARCH}.rpm"
-  LOCAL_REPO_PATH="${AWS_S3_MOUNTPOINT}${BASE_PATH}/${os_version}/${ARCH}"
+echo "===> Download RPM packages from GH"
+for arch in "${ARCH[@]}"; do
+  if [ $arch == 'x86_64' ]; then
+    package_name="nri-${INTEGRATION}-${TAG:1}-${SUFIX}.${arch}.rpm"
+  else
+    package_name="nri-${INTEGRATION}-${TAG:1}.${arch}.rpm"
+  fi
+  echo "===> Download ${package_name} from GH"
+  curl -SL https://github.com/${REPO_FULL_NAME}/releases/download/${TAG}/${package_name} -o ${package_name}
+done
 
-  echo "===> Downloading ${package_name} from GH"
-  wget --quiet https://github.com/${REPO_FULL_NAME}/releases/download/${TAG}/${package_name}
+for arch in "${ARCH[@]}"; do
+  for os_version in "${OS_VERSIONS[@]}"; do
+    if [ $arch == 'x86_64' ]; then
+      package_name="nri-${INTEGRATION}-${TAG:1}-${SUFIX}.${arch}.rpm"
+    else
+      package_name="nri-${INTEGRATION}-${TAG:1}.${arch}.rpm"
+    fi
+    LOCAL_REPO_PATH="${AWS_S3_MOUNTPOINT}${BASE_PATH}/${os_version}/${arch}"
+    echo "===> Creating local directory if not exists ${LOCAL_REPO_PATH}/repodata"
+    [ -d "${LOCAL_REPO_PATH}/repodata" ] || mkdir -p "${LOCAL_REPO_PATH}/repodata"
+    sleep 2 # Wait Fuse to sync
+    echo "===> Uploading ${package_name} to S3 in ${BASE_PATH}/${os_version}/${arch}"
+    cp ${package_name} ${LOCAL_REPO_PATH}
 
-  echo "===>Creating local directory if not exists ${LOCAL_REPO_PATH}/repodata"
-  [ -d "${LOCAL_REPO_PATH}/repodata" ] || mkdir -p "${LOCAL_REPO_PATH}/repodata"
-  sleep 3
+    echo "===> Delete and recreate metadata for ${package_name}"
+    find ${LOCAL_REPO_PATH} -regex '^.*repodata' | xargs -n 1 rm -rf
+    sleep 2 # Wait Fuse to sync
+    time createrepo --update -s sha "${LOCAL_REPO_PATH}"
+    FILE="${LOCAL_REPO_PATH}/repodata/repomd.xml"
+    while [ ! -f $FILE ];do
+       echo "===> Waiting repomd.xml exists..."
+       sleep 2 # Wait Fuse to sync
+    done
 
-  echo "===> Uploading ${package_name} to S3 in ${BASE_PATH}/${os_version}/${ARCH}"
-  cp ${package_name} ${LOCAL_REPO_PATH}
-
-  echo "===> Updating metadata for $package_name"
-  find ${LOCAL_REPO_PATH} -regex '^.*repodata' | xargs -n 1 rm -rf
-  sleep 3
-  time createrepo --update -s sha "${LOCAL_REPO_PATH}"
-  FILE="${LOCAL_REPO_PATH}/repodata/repomd.xml"
-  while [ ! -f $FILE ];do
-     echo "===> Waiting repomd.xml exists..."
-     sleep 5
+    echo "===> Updating GPG metadata dettached signature in ${BASE_PATH}/${os_version}/${arch}"
+    gpg --batch --pinentry-mode=loopback --passphrase ${GPG_PASSPHRASE} --detach-sign --armor "${LOCAL_REPO_PATH}/repodata/repomd.xml"
   done
-
-  echo "===>Updating GPG metadata dettached signature in ${BASE_PATH}/${os_version}/${ARCH}"
-  gpg --batch --pinentry-mode=loopback --passphrase ${GPG_PASSPHRASE} --detach-sign --armor "${LOCAL_REPO_PATH}/repodata/repomd.xml"
 done
