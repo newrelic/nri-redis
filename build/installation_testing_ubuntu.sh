41187f5eed085556a8ba38e136276d257741d3b9
Joan Porta <jportasa@gmail.com>
2020-10-29 09:59:20 +0100

    Typo in installation tests over Ubuntu

--- build/installation_testing_ubuntu.sh
+++ build/installation_testing_ubuntu.sh
@@ -7,14 +7,14 @@ set -e
 #
 sudo apt-get update
 sudo apt install gnupg curl -y
-curl -s "https://${AWS_S3_BUCKET}.s3.amazon.com/infrastructure_agent/gpg/newrelic-infra.gpg" | sudo apt-key add -
+curl -s "https://${AWS_S3_BUCKET}.s3.amazonaws.com/infrastructure_agent/gpg/newrelic-infra.gpg" | sudo apt-key add -
 
 echo "===> Production release installation over Ubuntu 18.04"
-printf "deb [arch=amd64] https://${AWS_S3_BUCKET}.s3.amazon.com/infrastructure_agent/linux/apt bionic main\n" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
+printf "deb [arch=amd64] https://${AWS_S3_BUCKET}.s3.amazonaws.com/infrastructure_agent/linux/apt bionic main\n" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
 sudo apt-get update
 sudo apt-get install newrelic-infra -y
 
 echo "===> Prerelease installation over Ubuntu 18.04"
-printf "deb [arch=amd64] https://${AWS_S3_BUCKET}.s3.amazon.com/infrastructure_agent/test/linux/apt bionic main\n" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
+printf "deb [arch=amd64] https://${AWS_S3_BUCKET}.s3.amazonaws.com/infrastructure_agent/test/linux/apt bionic main\n" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
 sudo apt-get update
 sudo apt-get install newrelic-infra -y
