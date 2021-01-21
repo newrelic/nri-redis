fbd7169bb23ed8fcff5294121b93ab99b574412c
David Brota <davidbrota@gmail.com>
2021-01-21 09:31:15 +0100

    wip cleaning up

--- actions/publish/start-script.sh
+++ actions/publish/start-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo $AWS_ACCESS_KEY:$AWS_SECRET_ACCESS_KEY > /etc/passwd-s3fs && chmod 600 /etc/passwd-s3fs
-env
-s3fs $S3_BUCKET_NAME $S3_MOUNT_DIRECTORY
-ls $S3_MOUNT_DIRECTORY
+s3fs $AWS_S3_BUCKET_NAME $AWS_S3_MOUNT_DIRECTORY
+pwd
+ls $AWS_S3_MOUNT_DIRECTORY
