36015772b9efd7b4b98ebaca36bff1d7763497a5
Grigorii Merkushev <brushknight@gmail.com>
2021-01-25 17:43:08 +0100

    wip: add draft of uploader.go

---
+++ actions/publish/uploader.go
@@ -0,0 +1,48 @@
+package main
+
+
+func main() {
+	// get version from env
+	version := "0.0.0"
+	// take schema
+	fileContent, err := readFileContent("filepath")
+
+	if err != nil{
+		// do something
+	}
+
+	//handle err
+	// parse schema
+	config := parseConfig(fileContent)
+	// iterate over config
+	uploadArtifacts(config, version)
+	// --> replace variables
+	// --> copy (move?) file
+}
+
+type uploadArtifactConfig struct {
+	src string `json:"src"`
+	dest string `json:"dest"`
+	arch []string `json:"arch"`
+}
+
+type uploadConfig uploadArtifactConfig
+
+func readFileContent(filePath string) (string, error){
+	return "", nil
+}
+
+func parseConfig(fileContent string) uploadConfig {
+	return uploadConfig{}
+}
+
+func uploadArtifact(config uploadArtifactConfig){}
+
+func uploadArtifacts(config uploadConfig, version string){}
+
+func replacePlaceholders(srcTemplate, destTemplate, arch, binaryName, version string) (string, string){
+
+	srcFileName := ""
+	destPath := ""
+	return srcFileName, destPath
+}
