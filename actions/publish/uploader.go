cb03e6ef5b2a81f665973ca38a94503fc41d81c9
Grigorii Merkushev <brushknight@gmail.com>
2021-01-26 10:55:28 +0100

    wip: uploader.go

--- actions/publish/uploader.go
+++ actions/publish/uploader.go
@@ -1,46 +1,99 @@
 package main
 
+import (
+	"github.com/prometheus/common/log"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
 	// get version from env
 	version := "0.0.0"
+	s3Folder := "path/to/s3"
+	artifactsStore := "path/to/downloads"
+	fileMappingConfig := "path/to/config"
+	binaryName := "some-app-name"
+
 	// take schema
 	fileContent, err := readFileContent("filepath")
 
-	if err != nil{
-		// do something
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	//handle err
 	// parse schema
 	config := parseConfig(fileContent)
 	// iterate over config
-	uploadArtifacts(config, version)
+	uploadArtifacts(binaryName, config, version)
 	// --> replace variables
 	// --> copy (move?) file
 }
 
 type uploadArtifactConfig struct {
-	src string `json:"src"`
-	dest string `json:"dest"`
+	src  string   `json:"src"`
+	dest string   `json:"dest"`
 	arch []string `json:"arch"`
 }
 
-type uploadConfig uploadArtifactConfig
+type uploadConfig []uploadArtifactConfig
 
-func readFileContent(filePath string) (string, error){
-	return "", nil
+func readFileContent(filePath string) ([]byte, error) {
+	fileContent, err := ioutil.ReadFile(filePath)
+
+	return fileContent, err
 }
 
-func parseConfig(fileContent string) uploadConfig {
-	return uploadConfig{}
+func parseConfig(fileContent []byte) (uploadConfig, error) {
+
+	var config uploadConfig
+
+	err := yaml.Unmarshal(fileContent, &config)
+
+	if err != nil {
+		return uploadConfig{}, err
+
+	}
+
+	return config, nil
 }
 
-func uploadArtifact(config uploadArtifactConfig){}
+func uploadArtifact(binaryName string, config uploadArtifactConfig, version string) {
 
-func uploadArtifacts(config uploadConfig, version string){}
+	if len(config.arch) > 0 {
 
-func replacePlaceholders(srcTemplate, destTemplate, arch, binaryName, version string) (string, string){
+		for _, arch := range config.arch {
+			srcPath, destPath := replacePlaceholders(config.src, config.dest, arch, binaryName, version)
+
+			//srcPath = os.Jo "" + srcPath
+
+			input, err := ioutil.ReadFile(srcPath)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+
+			err = ioutil.WriteFile(destPath, input, 0644)
+			if err != nil {
+				log.Error("Error creating", destPath)
+				log.Fatal(err)
+				return
+			}
+
+		}
+	} else {
+		replacePlaceholders(config.src, config.dest, "", binaryName, version)
+	}
+}
+
+func uploadArtifacts(binaryName string, config uploadConfig, version string) {
+	for _, artifactConfig := range config {
+		uploadArtifact(binaryName, artifactConfig, version)
+	}
+}
+
+func replacePlaceholders(srcTemplate, destTemplate, arch, binaryName, version string) (string, string) {
 
 	srcFileName := ""
 	destPath := ""
