bd91a816d40b3ad5c96d5630d180ab2803462bc5
David Brota <davidbrota@gmail.com>
2021-01-28 16:19:36 +0100

    wip missing dollar

--- actions/publish/action-run-ohi.sh
+++ actions/publish/action-run-ohi.sh
@@ -7,12 +7,12 @@ docker build -t newrelic/infrastructure-publish-action -f ./actions/publish/Dock
 docker run --rm --security-opt apparmor:unconfined \
         --device /dev/fuse \
         --cap-add SYS_ADMIN \
-        -e AWS_SECRET_ACCESS_KEY=INPUT_AWS_SECRET_ACCESS_KEY \
-        -e AWS_ACCESS_KEY=INPUT_AWS_ACCESS_KEY \
-        -e AWS_S3_BUCKET_NAME=INPUT_AWS_S3_BUCKET_NAME \
-        -e REPO_NAME=INPUT_REPO_NAME \
-        -e APP_NAME=INPUT_APP_NAME \
-        -e TAG=INPUT_TAG \
+        -e AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET_ACCESS_KEY \
+        -e AWS_ACCESS_KEY=$INPUT_AWS_ACCESS_KEY \
+        -e AWS_S3_BUCKET_NAME=$INPUT_AWS_S3_BUCKET_NAME \
+        -e REPO_NAME=$INPUT_REPO_NAME \
+        -e APP_NAME=$INPUT_APP_NAME \
+        -e TAG=$INPUT_TAG \
         -e ARTIFACTS_DEST_FOLDER=/mnt/s3 \
         -e ARTIFACTS_SRC_FOLDER=/home/gha/assets \
         -e UPLOADSCHEMA_FILE_PATH=/home/gha/schemas/ohi.yml \
