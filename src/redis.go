0135b5ce1014855c033e60f8fcbcfc507750997d
paologallinaharbur <paologallina1992@gmail.com>
2021-08-06 09:55:49 +0200

    fix(argsValidation): remove strict args validation

--- src/redis.go
+++ src/redis.go
@@ -47,9 +47,7 @@ var (
 	gitCommit          = ""
 	buildDate          = ""
 
-	errorArgsNetworkPort     = errors.New("UseUnixSocket is false, but port is empty")
-	errorArgsNetworkHostname = errors.New("UseUnixSocket is false, but hostname is empty")
-	errorArgsUnixSocket      = errors.New("UseUnixSocket is true, but unixSocket is empty")
+	errorArgs = errors.New("no connection method available, UnixSocketPath or Hostname and Port should be populated")
 )
 
 func main() {
@@ -61,16 +59,13 @@ func main() {
 		os.Exit(0)
 	}
 
-	err = validateArgs()
-	fatalIfErr(err)
-
 	dialOptions := standardDialOptions(args.Password)
 
 	var c conn
-	if args.UseUnixSocket {
+	if args.UseUnixSocket && args.UnixSocketPath != "" {
 		c, err = newSocketRedisCon(args.UnixSocketPath, dialOptions...)
 		fatalIfErr(err)
-	} else {
+	} else if args.Hostname != "" && args.Port > 0 {
 		if args.UseTLS {
 			tlsDialOptions := tlsDialOptions(args.UseTLS, args.TLSInsecureSkipVerify)
 			dialOptions = append(dialOptions, tlsDialOptions...)
@@ -78,7 +73,10 @@ func main() {
 		redisURL := net.JoinHostPort(args.Hostname, strconv.Itoa(args.Port))
 		c, err = newNetworkRedisCon(redisURL, dialOptions...)
 		fatalIfErr(err)
+	} else {
+		log.Fatal(errorArgs)
 	}
+
 	defer c.Close()
 
 	// Support using renamed form of redis commands, if 'renamed-command' config is used in Redis server
@@ -148,22 +146,6 @@ func main() {
 	}
 
 	fatalIfErr(i.Publish())
-}
-
-func validateArgs() error {
-	if args.UseUnixSocket && args.UnixSocketPath == "" {
-		return errorArgsUnixSocket
-	}
-
-	if !args.UseUnixSocket && args.Hostname == "" {
-		return errorArgsNetworkHostname
-	}
-
-	if !args.UseUnixSocket && args.Port == 0 {
-		return errorArgsNetworkPort
-	}
-
-	return nil
 }
 
 func printVersion() {
